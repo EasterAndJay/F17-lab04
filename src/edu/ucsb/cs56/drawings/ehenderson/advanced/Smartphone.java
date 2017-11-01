998af0bf8dfbd9302f6c294298e3c14796b852bc
Ethan Henderson <ehenderson@umail.ucsb.edu>
2017-11-01 15:06:13 -0700

    Updated Smartphone.java

--- src/edu/ucsb/cs56/drawings/ehenderson/advanced/Smartphone.java
+++ src/edu/ucsb/cs56/drawings/ehenderson/advanced/Smartphone.java
@@ -22,8 +22,8 @@ public class Smartphone extends GeneralPathWrapper implements Shape
     /**
        Constructor
        
-       @param x x coord of lower left corner of Smartphone
-       @param y y coord of lower left corner of Smartphone
+       @param x x coord of upper left corner of Smartphone
+       @param y y coord of upper left corner of Smartphone
        @param width width of the Smartphone
        @param height of Smartphone
     */
@@ -33,38 +33,30 @@ public class Smartphone extends GeneralPathWrapper implements Shape
         // Rather than having to scale at the end, we can just
         // draw things the right way to begin with, using the
         // x, y, width and height.   .
-        
-        double screenHeight = .70 * height;
+       	y = 500 - y; 
+        double screenHeight = .80 * height;
         double splitHeightY = (height - screenHeight)/2;
 	double splitLengthX = .05 * width;
-        double screenUpperLeftY = (y - height)+ splitHeightY;
-        double phoneHeight = screenUpperLeftY - (splitHeightY/5);
+        double screenUpperLeftY = y+ (.1*height) ;
         
         // Make the Smartphone screen
         
         Rectangle2D.Double screen = 
             new Rectangle2D.Double(x + splitLengthX, screenUpperLeftY ,
-				   width-(2*splitLengthX), y - (2*splitHeightY));
+				   width-(2*splitLengthX), screenHeight);
 	
         // make the Smartphone body.
         
         Rectangle2D.Double smartphone = 
-	    new Rectangle2D.Double(x, phoneHeight, width, height);	
+	    new Rectangle2D.Double(x, y, width, height);	
 
-	//Make the camera
-	
-	Circle camera = new Circle ((x+(width/2)), phoneHeight + 10, (.35 * (splitHeightY/6)));
-
-	//Make the home button
-	
-	Circle home = new Circle ((x+(width/2)), y+splitHeightY-(splitHeightY/3), (.5 * (splitHeightY/6)));
 	
         // put the whole Smartphone together
 	
         GeneralPath wholePhone = this.get();
         wholePhone.append(screen, false);
         wholePhone.append(smartphone, false);
-	wholePhone.append(camera, false);
-	wholePhone.append(home, false);
-    }
+
+
+    }      
 }

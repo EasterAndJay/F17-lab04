45050398e613bd55653feaf5a5bc9df42c0ff944
Ethan Henderson <ehenderson@umail.ucsb.edu>
2017-11-01 20:20:22 -0700

    Final Samsung class

--- src/edu/ucsb/cs56/drawings/ehenderson/advanced/Samsung.java
+++ src/edu/ucsb/cs56/drawings/ehenderson/advanced/Samsung.java
@@ -19,9 +19,14 @@ import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
 */
 public class Samsung extends Smartphone implements Shape
 {
+
+	double X;
+	double Y;
+	double Width;
+	double Height;
     /**
        Constructor
-       
+
        @param x x coord of upper left corner of phone
        @param y y coord of upper left corner of phone
        @param width width of the phone
@@ -30,25 +35,70 @@ public class Samsung extends Smartphone implements Shape
     public Samsung(double x, double y, double width, double height)
     {
 	
-        // Rather than having to scale at the end, we can just
-        // draw things the right way to begin with, using the
-        // x, y, width and height.   If you haven't already
-        // hard coded a particular drawing, this may be an easier
-        // way.
         super(x, y, width, height); 
+	X = x;
+	Y = 480-y;
+	Width = width;
+	Height = height;
 
-	Rectangle2D.Double home = new Rectangle2D.Double(50, 50, 100, 100);
-       
 	GeneralPath gp = this.get();
-        
-	
-        //  Remember that y goes DOWN the page,
-        // so we ADD to y to get a "lower" value on the screen
-        
-	
-        // put the whole Galaxy S7 together
-	
-        GeneralPath wholePhone = this.get();
-	wholePhone.append(home, false);
     }
+
+    public void addCamera() {
+	
+	double camX = X + (.75*Width);
+	double camY = Y + (.05*Height);
+    	double camR = .02*Height;
+
+	Circle camera = new Circle(camX, camY, camR);
+
+	GeneralPath gp = this.get();
+	gp.append(camera, false);
+    }
+
+    public void addHome() {
+	
+	double homeX = X + (Width/3);
+	double homeY = Y + (.925*Height);
+	double homeW = (Width/3);
+	double homeH = (.05*Height);
+
+	Rectangle2D.Double home = new Rectangle2D.Double(homeX, homeY, homeW, homeH);
+
+	GeneralPath gp = this.get();
+	gp.append(home, false);
+    }
+
+    public void addFlash() {
+	
+	double fx1 = X + (.20*Width);
+	double fy1 = Y + (.03*Height);
+	double fr1 = .01*Height;
+
+	Circle flash1 = new Circle(fx1, fy1, fr1);
+
+	double fx2 = fx1 + (3*fr1);
+	double fy2 = fy1;
+	double fr2 = fr1;
+
+	Circle flash2 = new Circle(fx2, fy2, fr2);
+
+	GeneralPath gp = this.get();
+	gp.append(flash1, false);
+	gp.append(flash2, false);
+    }
+    
+    public void addSpeaker() {
+	
+	double sx = X + (Width/3);
+	double sy = Y + (.03*Height);
+	double sW = (.25*Width);
+	double sH = .012 * Height;
+
+	Rectangle2D.Double speaker = new Rectangle2D.Double(sx, sy, sW, sH);
+
+	GeneralPath gp = this.get();
+	gp.append(speaker, false);
+    }
+
 }
